9312bd1d4ff39cb9b4ef9c25d59425d1ff6f4772
Rogerio Azevedo <rogerio.francis@gmail.com>
2021-05-02 22:56:32 -0400

    subscription canceled

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -21,7 +21,12 @@ export const config = {
   },
 }
 
-const relevantEvents = new Set(["checkout.session.completed"])
+const relevantEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.created",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+])
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   if (request.method === "POST") {
@@ -45,12 +50,25 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+            const subscription = event.data.object as Stripe.Subscription
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            )
+
+            break
+
           case "checkout.session.completed":
             const checkoutSession = event.data.object as Stripe.Checkout.Session
 
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             )
 
             break
@@ -60,8 +78,6 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
       } catch (err) {
         return response.json({ error: "Webhook handler failed" })
       }
-
-      console.log("Evento recebido", event)
     }
 
     response.json({ received: true })
