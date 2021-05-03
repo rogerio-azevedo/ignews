4f4f59deac464c5acf94290020e0bc3474501861
Rogerio Azevedo <rogerio.francis@gmail.com>
2021-05-02 20:47:13 -0400

    add checkout

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
 
-import { fauna } from "../../services/fauna"
+import { fauna } from "../../../services/fauna"
 import { query as q } from "faunadb"
 
 export default NextAuth({
@@ -16,6 +16,7 @@ export default NextAuth({
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user
+
       try {
         await fauna.query(
           q.If(
