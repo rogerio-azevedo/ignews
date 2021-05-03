c0f7682b001683dee381e5913e849f9e0a7497d2
Rogerio Azevedo <rogerio.francis@gmail.com>
2021-05-02 21:34:02 -0400

    add checkout log

---
+++ src/pages/posts.tsx
@@ -0,0 +1,14 @@
+// import { Container } from './styles';
+import React from "react"
+
+// import { Container } from './styles';
+
+const pages = () => {
+  return (
+    <div>
+      <h1>Posts</h1>
+    </div>
+  )
+}
+
+export default pages
